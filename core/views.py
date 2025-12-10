847410a5f374bbd72b00a37a24fe560aa9dd31a0
xNekros1 <128653561+xNekros1@users.noreply.github.com>
2025-12-09 23:34:44 -0300

    Add messages import to views.py

--- core/views.py
+++ core/views.py
@@ -3,6 +3,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 from django.http import JsonResponse
 import json
 from django.db.models import Sum
